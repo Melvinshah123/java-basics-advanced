d2fcb9df40c90de7aa1d3db98aa446ef43bca46d
Melvinshah123 <118537500+Melvinshah123@users.noreply.github.com>
2025-06-17 16:00:24 +0530

    conditionals + ternary operator

---
+++ course/Tutorial1.java
@@ -0,0 +1,23 @@
+public class Tutorial1 {
+    public static void main(String[] args) 
+    {
+        int x = 5;
+        int y = 3;
+
+        boolean result = x < y;
+        System.out.println(result);
+
+        int a = 8;
+        // if (a <= 8) 
+        // {
+        //     System.out.println("Hello");
+        // } 
+        
+        // else 
+        // {
+        //     System.out.println("bye");
+        // }
+
+        System.out.println(a >= 8 ? "Hello":"bye"); 
+    }
+}
