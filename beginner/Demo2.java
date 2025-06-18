bbf20be54dcbd815962cbc220cca84a964d63901
Melvinshah123 <118537500+Melvinshah123@users.noreply.github.com>
2025-06-18 15:08:36 +0530

    method overloading + instance variable

--- beginner/Demo2.java
+++ beginner/Demo2.java
@@ -1,5 +1,6 @@
 class Calc 
     {
+        int num = 5; //Instance Variable
         public int add(int n1 , int n2)
         {
             return n1 + n2;
@@ -16,9 +17,16 @@ public class Demo2
    public static void main(String[] args) 
    {
         Calc obj = new Calc();
-        int result1 = obj.add(1, 2);
-        int result2 = obj.add(1,2,3);
-        System.out.println(result1);
-        System.out.println(result2);
-   }
+        Calc obj1 = new Calc();
+        // int result1 = obj.add(1, 2);
+        // int result2 = obj.add(1,2,3);
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
+        obj.num = 7;
+      //System.out.println(result1);
+      //System.out.println(result2);
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
+    }
 }
+
